aaa1d7d5ad4506db4f1869bab706c8eeaa60175c
Ismay1l <96645334+Ismay1l@users.noreply.github.com>
2022-09-07 17:03:09 +0400

    latest version

--- MyMusic/App/Seach/View/Cells/SearchCategoriesCollectionViewCell.swift
+++ MyMusic/App/Seach/View/Cells/SearchCategoriesCollectionViewCell.swift
@@ -9,4 +9,17 @@ import UIKit
 
 class SearchCategoriesCollectionViewCell: UICollectionViewCell {
     
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    //MARK: Parent Delegate
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    //MARK: Functions
+    private func setupUI() {
+    }
 }
